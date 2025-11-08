2f47995e6afe4d630d36a8e55ae40bc1869198a3
ismailkmr <ismail.tk18@gmail.com>
2025-11-08 19:08:09 +0530

    Initial commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend running successfully! Ismail................");
+  res.send(333*8);
 });
 
 const PORT = process.env.PORT || 5000;
